cbf3c4d20358df9260ced2a93700a4f81daf789c
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2025-05-16 15:31:30 -0500

    Update drake.py

--- drake.py
+++ drake.py
@@ -1,102 +1,92 @@
-import neopixel
+import time
 import random
 import board
+import neopixel
+import random
 import touchio
-import time
-#deck info from https://memory-alpha.fandom.com/wiki/Constitution_class_decks
-#dests info from https://exoplanets.nasa.gov/news/1378/top-10-star-trek-destinations-chosen-by-nasa-scientists/
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=True)
+from ncount import *
+from prt import *
+from wise import *
+
+#based on https://github.com/DeaconDesperado/pydrake/blob/master/drake.py
+#
+# see https://en.wikipedia.org/wiki/Drake_equation
+
+# set up touch for input
 touch1 = touchio.TouchIn(board.TOUCH1)
 touch2 = touchio.TouchIn(board.TOUCH2)
 
-from prt import *
+
+
+def placeNM():
+    #build a random star name 
+    start = ["Gi","Ro","Ah","Mi","Pa","Ki","Re","Sy","Th","Zo"]
+    mid = ["eu","af","gh","uu","a","e","i","o","u","y"]
+    end = ["nk","as","of","z","d","n","ll","ah","ei","ie"]
+
+    Place = random.choice(start)+random.choice(mid)+random.choice(end)
+    return (Place)
+
 REPL = True
-prt("Trek",REPL)
-from wise import *
 
+def drake():
+	R = 1E11
+	Fp = random.uniform(0.1,0.3)
+	Ne = random.randrange(1,5)
+	Fl = random.uniform(0.1,0.5)
+	Fi = random.uniform(0.01,0.2)
+	Fc = random.uniform(0.01,0.2)
+	L = daysearth()
+	prt (('Number of stars in the Milky Way '+ str(R)),REPL)
+	prt (('Fraction of stars with planetary systems '+str(Fp)),REPL)
+	prt (('Number of planets suitable for life '+ str(Ne)),REPL)
+	prt (('Fraction of suitable planets that develop life '+  str(Fl)),REPL)
+	prt (('Fraction of life that becomes sentient and eventually intelligent '+ str(Fi)),REPL)
+	prt (('Fraction of intelligent life that advances to the point of communication '+ str(Fc)),REPL)
+	prt (('Adjustment for incidence of self-destruction '+ str(L)),REPL)
+	N = int((R*Fp*Ne*Fl*Fi*Fc*L)+.5)
+	prt (("Possible number of civilizations: " + str(N)+"\n\n"),REPL)
+	return N
 
-#Define colors
-pink = (12,10,12)
-gold = (50, 40, 5)
-blue = (0,0,8)
-orange = (25, 10, 0)
-blank = (0,0,0)
-grn = (0,20,0)
-green  = (0,20,0)
-red = (20,0,0)
-paleblue = (0,0,1)
-white = (20,20,20)
-purple = (20,0,30)
+def daysearth():
+	age = 4.54E7
+	indays = age*365
+	cc = 50*365
+	L = cc/indays
+	return L
 
+if (not REPL):
+        Val = 0
+        while Val == 0 :
+                compthink()
+                if touch1.value:
+                        Val = Val +1
+                if touch2.value:
+                        Val = Val +2
+                        
+count = drake()
 
-colors = [pink,gold,blue,orange,green,red,paleblue,white,purple]
+prt(getWD('sagan'),REPL)
 
-def docolor(color): #show a color  briefly
-    for i in range(4):
-        pixels[i] = color
-
-
-    pixels.show()
-    time.sleep(.25)
-
-    for i in range(4):
-        pixels[i] = blank
-
-    pixels.show()
-
-
-def blinknum(num,color): #count out a number in a color
-    for i in range(num):
-        docolor(color)
-        time.sleep(.1)
-
-
-def compthink(): #blink out all the colors when computer "thinking"
-    for i in range(random.randrange(3,5)):
-        blinknum(1,random.choice(colors))
-
-
-
-for i in range(file_len("decks")):
-    dk = str(i+1)+": "+wisdom(i,"decks")
-    dk = dk.rstrip()
-    prt(dk,REPL)
-
-def PickDest():
-    dest = wisdom(random.randrange(file_len("dests")),"dests").rstrip()
-    ETA = random.randrange(3,8)
-    return (dest,ETA)
-
-
-Deck = 0 # start at bridge
-(dest,ETA) = PickDest()
-
-prt (("Ship destination is: " +dest) ,REPL)
 while True:
-    prt (("You are on deck: "+ str(Deck + 1)+ " " + wisdom(Deck,"decks")).rstrip(),REPL)
-    compthink()
-    val = 0
-    while val ==0:
-        val = 0
-        if touch1.value:
-            val = val+1
-        if touch2.value:
-            val = val+2
-    ETA = ETA - 1
-    if ETA <= 0:
-        prt("Arrived at "+dest,REPL)
-        (dest,ETA)=PickDest()
-        prt (("Ship new destination is: " +dest) ,REPL)
 
+    Val = 0
+    if touch1.value:
+        Val = Val +1
+        touched = time.monotonic()
+    if touch2.value:
+        Val = Val +2
+        touched = time.monotonic()
 
-    if val == 1:
-        Deck = Deck - 1
-        if Deck < 0:
-            Deck = 0
-    if val == 2:
-        Deck = Deck + 1
-        if Deck > 22:
-            Deck = 22
-    if val == 3:
-        Deck = random.randrange(file_len("decks"))
+    if Val == 1: #calculate count
+        compthink()
+        count = drake()
+        prt(getWD('sagan'),REPL)
+        
+
+    if Val == 2 : #display places
+        blinknum(1,blue)
+        prt("0: Earth",REPL)
+        for i in range(count):
+            prt((str(i+1)+": "+placeNM()),REPL)
     time.sleep(.25)
